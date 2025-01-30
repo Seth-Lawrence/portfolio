ea196278c187f24e192aa33357001b5bde5548f8
Seth Hobson <sethlawrencehobson@gmail.com>
2025-01-29 23:53:11 -0500

    added type and props

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,20 +1,28 @@
-export default function ProjectCard() {
+type ProjectCardProps = {
+    name: string;
+    image: string;
+    description: string;
+};
+
+const ProjectCard = (project: ProjectCardProps) => {
 
     return (
         <div className='ProjectCard'>
             <table>
-               <tr className="ProjectCardHeader">
-               <th>Project 1 name</th>
-               </tr>
-               <tr>
-                <img className="projectCardImage" alt="project image"></img>
-               </tr>
-               <tr>
-                <td className="ProjectCardDescription">
-                    Project Description
-                </td>
-               </tr>
+                <tr className="ProjectCardHeader">
+                    <th>{project.name}</th>
+                </tr>
+                <tr>
+                    <img className="projectCardImage" alt="project image" src={project.image}></img>
+                </tr>
+                <tr>
+                    <td className="ProjectCardDescription">
+                        {project.description}
+                    </td>
+                </tr>
             </table>
         </div>
-    )
-}
+    );
+};
+
+export default ProjectCard;
