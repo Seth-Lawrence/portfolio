ae6c730c2423b4b0c0dfd71bf0c6f1331f580693
Seth Hobson <sethlawrencehobson@gmail.com>
2025-01-29 23:22:06 -0500

    added projectcard skeleton

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,19 @@ export default function ProjectCard() {
 
     return (
         <div className='ProjectCard'>
-
+            <table>
+               <tr className="ProjectCardHeader">
+               <th>Project 1 name</th>
+               </tr>
+               <tr>
+                <img className="projectCardImage" alt="project image"></img>
+               </tr>
+               <tr>
+                <td className="ProjectCardDescription">
+                    Project Description
+                </td>
+               </tr>
+            </table>
         </div>
     )
 }
